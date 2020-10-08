33025d7dbf788a15efd5b805f0c71a9815e1e459
muhammadhamdits <muhammadhamdi702@gmail.com>
2020-10-08 10:16:52 +0700

    gg

--- resources/views/user/storeDetail.blade.php
+++ resources/views/user/storeDetail.blade.php
@@ -9,7 +9,7 @@
                         <div class="card" style="width: 100%">
                             <div class="gambar" style="background-image: url('/img/shop/{{ $store->pict_1 }}')"></div>
                         </div>
-                        {{ $store->name }}
+                        {{ $store->shop_name }}
                     </div>
                 </div>
             </div>
@@ -31,7 +31,7 @@
                             @endforeach
                         </div>
                     </div>
-                    <div class="card-footer">
+                    <!-- <div class="card-footer">
                         <nav aria-label="Store List Page Navigation">
                             <ul class="pagination justify-content-center m-0">
                                 <li class="page-item active"><a class="page-link" href="#">1</a></li>
@@ -40,7 +40,7 @@
                                 <li class="page-item"><a class="page-link" href="#">4</a></li>
                             </ul>
                         </nav>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
